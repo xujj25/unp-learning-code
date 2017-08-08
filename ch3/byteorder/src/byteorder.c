f536e657a7f93719da95a9498102604174eeff9a
xujj25 <xujj25@foxmail.com>
2017-08-08 16:26:28 +0800

    add ch4

---
+++ ch3/byteorder/src/byteorder.c
@@ -0,0 +1,22 @@
+#include "unp.h"
+
+int main(int argc, char const *argv[])
+{
+	union {
+		short s;
+		char c[sizeof(short)];
+	} un;
+
+	un.s = 0x0102;
+	printf("%s: ", CPU_VENDOR_OS);
+	if (sizeof(short) == 2) {
+		if (un.c[0] == 1 && un.c[1] == 2)
+			printf("big-endian\n");
+		else if (un.c[0] == 2 && un.c[1] == 1)
+			printf("little-endian\n");
+		else
+			printf("unknown\n");
+	} else
+		printf("sizeof(short) = %lu\n", sizeof(short));
+	exit(0);
+}
