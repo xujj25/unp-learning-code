569c17e6ed6a00d8c32850c0e80db4f6e0e30ed0
xujj25 <xujj25@foxmail.com>
2017-08-20 00:27:12 +0800

    modify str_echo

--- ch5/echo_cs/include/str_echo.h
+++ ch5/echo_cs/include/str_echo.h
@@ -1,15 +1,36 @@
 #include "unp.h"
 
+// void str_echo(int sockfd)
+// {
+//     ssize_t n;
+//     char buf[MAXLINE];
+// again:
+//     while((n = read(sockfd, buf, MAXLINE)) > 0)
+//         Writen(sockfd, buf, n);
+
+//     if (n < 0 && errno == EINTR)
+//         goto again;
+//     else if (n < 0)
+//         err_sys("str_echo: read error");
+// }
+
+/* 
+new str_echo: calculate sum of 2 input numbers
+*/
 void str_echo(int sockfd)
 {
-    ssize_t n;
-    char buf[MAXLINE];
-again:
-    while((n = read(sockfd, buf, MAXLINE)) > 0)
-        Writen(sockfd, buf, n);
+	long arg1, arg2;
+	ssize_t n;
+	char line[MAXLINE];
 
-    if (n < 0 && errno == EINTR)
-        goto again;
-    else if (n < 0)
-        err_sys("str_echo: read error");
+	for ( ; ; ) {
+		if ((n = Readline(sockfd, line, MAXLINE)) == 0)
+			return;  // connection closed by other end
+		if (sscanf(line, "%ld%ld", &arg1, &arg2) == 2)
+			snprintf(line, sizeof(line), "%ld\n", arg1 + arg2);
+		else
+			snprintf(line, sizeof(line), "input error\n");
+		n = strlen(line);
+		Writen(sockfd, line, n);
+	}
 }
